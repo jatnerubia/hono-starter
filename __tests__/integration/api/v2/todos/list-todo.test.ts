4149adf09b605050642b3df3c06ff4e3d20c4d61
jatnerubia <jasonlerit077@gmail.com>
2024-10-26 18:56:09 +0800

    update: list todo test

--- __tests__/integration/api/v2/todos/list-todo.test.ts
+++ __tests__/integration/api/v2/todos/list-todo.test.ts
@@ -1,17 +1,12 @@
 import { v2TodosRoute } from "@/api/v2/todos"
 import { HttpStatusCodes } from "@/common/constants/http-status.constant"
 import { createApp } from "@/common/create-app"
-import { execSync } from "child_process"
 import { testClient } from "hono/testing"
-import { beforeAll, describe, expect, expectTypeOf, it } from "vitest"
+import { describe, expect, expectTypeOf, it } from "vitest"
 
 const client = testClient(createApp().route("/", v2TodosRoute))
 
 describe("List Todo API", () => {
-  beforeAll(async () => {
-    execSync("npm run db:push")
-  })
-
   it("should return all todos", async () => {
     const response = await client.todos.$get()
     const result = await response.json()
