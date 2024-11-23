160ff095f004586fe3f361d040241ff37ae0f75e
jatnerubia <jasonlerit077@gmail.com>
2024-11-23 10:25:13 +0800

    update: users schema

--- src/db/migrations/0000_lovely_nextwave.sql
+++ src/db/migrations/0000_lovely_nextwave.sql
@@ -7,10 +7,11 @@ CREATE TABLE IF NOT EXISTS "todos" (
 );
 --> statement-breakpoint
 CREATE TABLE IF NOT EXISTS "users" (
-	"id" bigserial PRIMARY KEY NOT NULL,
-	"email" varchar(255) NOT NULL,
-	"password" varchar(255) NOT NULL,
-	"full_name" varchar(255) NOT NULL,
+	"id" uuid PRIMARY KEY DEFAULT gen_random_uuid() NOT NULL,
+	"name" text,
+	"email" text,
+	"password" text,
+	"email_verified_at" timestamp,
 	"created_at" timestamp DEFAULT now(),
 	"updated_at" timestamp DEFAULT now(),
 	"deleted_at" timestamp,
--- src/db/schemas/users.schema.ts
+++ src/db/schemas/users.schema.ts
@@ -1,11 +1,12 @@
-import { bigserial, pgTable, timestamp, varchar } from "drizzle-orm/pg-core"
+import { pgTable, text, timestamp, uuid } from "drizzle-orm/pg-core"
 import { createInsertSchema, createSelectSchema } from "drizzle-zod"
 
 export const users = pgTable("users", {
-  id: bigserial({ mode: "number" }).primaryKey(),
-  email: varchar({ length: 255 }).notNull().unique(),
-  password: varchar({ length: 255 }).notNull(),
-  fullName: varchar({ length: 255 }).notNull(),
+  id: uuid().primaryKey().defaultRandom(),
+  name: text(),
+  email: text().unique(),
+  password: text(),
+  emailVerifiedAt: timestamp(),
   createdAt: timestamp().defaultNow(),
   updatedAt: timestamp()
     .defaultNow()
@@ -18,11 +19,12 @@ export const selectUsersSchema = createSelectSchema(users).omit({
 })
 
 export const insertUsersSchema = createInsertSchema(users, {
+  name: (s) => s.name.min(1).max(255),
   email: (s) => s.email.email().max(255),
   password: (s) => s.password.min(8).max(255),
-  fullName: (s) => s.fullName.min(1).max(255),
 }).omit({
   id: true,
+  emailVerifiedAt: true,
   createdAt: true,
   updatedAt: true,
   deletedAt: true,
